176e5fa5433ffe1049c12b5b76039ea44b4d1c68
Abraklion <abraklion@gmail.com>
2023-06-13 20:05:45 +0500

    Настройка сборки

---
+++ docs/js/main.js
@@ -0,0 +1,19 @@
+(self["webpackChunkgulp_html_scss"] = self["webpackChunkgulp_html_scss"] || []).push([["main"],{
+
+/***/ "./src/js/main.js":
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+/***/ (function() {
+
+console.log('ok');
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/js/main.js"));
+/******/ }
+]);
+//# sourceMappingURL=main.js.map
---
+++ gulp/tasks/scripts.js
@@ -0,0 +1,55 @@
+// Обьединяет js файлы в один и переносит файл в папку dist/js
+
+module.exports = function () {
+
+  $.gulp.task("scripts", () => {
+
+    return $.gulp.src($.config.paths.js + '*.js')
+      .pipe($.webpack({
+        mode: $.config.toggle.mode,
+        entry: {
+          // каркас общий
+          main: {
+            import: './src/js/main.js'
+          },
+
+          // главная страница
+          index: {
+            import: './src/js/index.js'
+          },
+        },
+        output: {
+          filename: '[name].js'
+        },
+        optimization: {
+          runtimeChunk: 'single',
+          // splitChunks: {
+          //   chunks: 'all',
+          // },
+        },
+        watch: false,
+        devtool: 'source-map',
+        module: {
+          rules: [
+            {
+              test: /\.m?js$/,
+              exclude: /(node_modules|bower_components)/,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: [['@babel/preset-env', {
+                    debug: true,
+                    corejs: 3,
+                    useBuiltIns: "usage"
+                  }]]
+                }
+              }
+            }
+          ]
+        }
+      }))
+      .pipe($.gulp.dest($.config.output.pathJs))
+      .on("end", $.browserSync.reload);
+
+  });
+}
